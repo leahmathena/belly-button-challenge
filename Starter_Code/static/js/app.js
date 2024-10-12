5d33c027fca7a88ad788e3d4e191323695576d45
leahmathena <leahashleymann@gmail.com>
2024-10-11 22:58:17 -0500

    Adding app.js code

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -3,20 +3,22 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    const metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    const result = metadata.find(meta => meta.id == sample);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    const metadataPanel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    metadataPanel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.entries(result).forEach(([key, value]) => {
+        metadataPanel.append("h6").text(`${key}: ${value}`);
+      });
   });
 }
 
@@ -25,29 +27,62 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    const samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    const sampleData = samples.find(s => s.id === sample);
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    const otu_ids = sampleData.otu_ids;
+        const otu_labels = sampleData.otu_labels;
+        const sample_values = sampleData.sample_values;
 
     // Build a Bubble Chart
+    const bubbleTrace = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: 'Earth'
+      }
+  };
 
+  const bubbleLayout = {
+      title: 'Bubble Chart of OTUs',
+      xaxis: { title: 'OTU ID' },
+      yaxis: { title: 'Sample Values' },
+      hovermode: 'closest'
+  };
 
     // Render the Bubble Chart
-
+    Plotly.newPlot('bubble', [bubbleTrace], bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    const top10Values = sample_values.slice(0, 10).reverse();
+        const top10Ids = otu_ids.slice(0, 10).reverse().map(id => `OTU ${id}`);
+        const top10Labels = otu_labels.slice(0, 10).reverse();
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    const barTrace = {
+      x: top10Values,
+      y: top10Ids,
+      text: top10Labels,
+      type: 'bar',
+      orientation: 'h'
+  };
 
+  const barLayout = {
+      title: `Top 10 OTUs for Test Subject ${sample}`,
+      xaxis: { title: 'Sample Values' },
+      yaxis: { title: 'OTU IDs' }
+  };
 
     // Render the Bar Chart
-
+    Plotly.newPlot('bar', [barTrace], barLayout);
   });
 }
 
@@ -56,28 +91,35 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
+    const sampleNames = data.names;
 
 
     // Use d3 to select the dropdown with id of `#selDataset`
+    const dropdown = d3.select("#selDataset");
 
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+    sampleNames.forEach(name => {
+      dropdown.append("option").text(name).property("value", name);
+  });
 
     // Get the first sample from the list
+    const firstSample = sampleNames[0];
 
 
     // Build charts and metadata panel with the first sample
-
+    buildMetadata(firstSample);
+    buildCharts(firstSample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildMetadata(newSample);
+  buildCharts(newSample);
 }
 
 // Initialize the dashboard
